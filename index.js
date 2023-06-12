b8ef09ef6bf71e09d87582e51241b6ce1a3b0116
CBB <cyf.pink@qq.com>
2023-06-12 16:39:05 +0800

    加入滚动锁定

--- index.js
+++ index.js
@@ -1,81 +1,100 @@
 /*
  *Author: CBB
  *Date: 2023-06-12 15:00:31
- *LastEditTime: 2023-06-12 15:38:34
+ *LastEditTime: 2023-06-12 16:35:52
  *LastEditors: CBB
  *Description:
  *FilePath: \DragTouch\index.js
  */
+/*******
+ * @description: body滚动开关
+ * @param {Boolean} flag
+ * @return {*}
+ */
+const bodyScroll = (flag) => {
+  if (flag) {
+    document.body.style.overflow = 'inherit'
+  } else {
+    document.body.style.overflow = 'hidden'
+  }
+}
 const DragTouch = {
   mounted(el) {
-    let x = 0;
-    let y = 0;
-    let isDragging = false;
-    let prevX = 0;
-    let prevY = 0;
+    let x = 0
+    let y = 0
+    let isDragging = false
+    let prevX = 0
+    let prevY = 0
 
-    const { width: screenWidth, height: screenHeight } =
-      document.body.getBoundingClientRect();
+    const {
+      width: screenWidth,
+      height: screenHeight,
+    } = document.body.getBoundingClientRect()
 
     const setElStyle = () => {
-      el.style.left = x + 'px';
-      el.style.top = y + 'px';
-    };
+      el.style.left = x + 'px'
+      el.style.top = y + 'px'
+    }
 
     const onTouchStart = (event) => {
-      if (isDragging) return;
-      isDragging = true;
-      const { left, top } = el.getBoundingClientRect();
-      x = left;
-      y = top;
-      const touch = event.touches[0];
-      prevX = touch.clientX - left;
-      prevY = touch.clientY - top;
-      el.dispatchEvent(new CustomEvent('isMove', { detail: true }));
-    };
+      if (isDragging) return
+      isDragging = true
+
+      const { left, top } = el.getBoundingClientRect()
+      x = left
+      y = top
+      const touch = event.touches[0]
+      prevX = touch.clientX - left
+      prevY = touch.clientY - top
+      el.dispatchEvent(new CustomEvent('isMove', { detail: true }))
+    }
 
     const onTouchMove = (event) => {
-      if (!isDragging) return;
-      event.preventDefault();
-      el.style.transition = 'none';
-      const { width, height } = el.getBoundingClientRect();
+      if (!isDragging) return
+      // 阻止页面滚动
+      bodyScroll(false)
+      event.preventDefault()
+      el.style.transition = 'none'
+      const { width, height } = el.getBoundingClientRect()
 
-      const touch = event.touches[0];
-      const offsetX = touch.clientX - prevX;
-      const offsetY = touch.clientY - prevY;
+      const touch = event.touches[0]
+      const offsetX = touch.clientX - prevX
+      const offsetY = touch.clientY - prevY
 
-      x = Math.max(0, Math.min(offsetX, screenWidth - width));
-      y = Math.max(0, Math.min(offsetY, screenHeight - height));
-      setElStyle();
-    };
+      x = Math.max(0, Math.min(offsetX, screenWidth - width))
+      y = Math.max(0, Math.min(offsetY, screenHeight - height))
+      setElStyle()
+    }
 
     const onTouchEnd = (event) => {
-      if (!isDragging) return;
-      isDragging = false;
-      el.style.transition = 'all 0.3s ease-out';
-      const { left, width } = el.getBoundingClientRect();
-      const bodyWidth = document.body.getBoundingClientRect().width;
-      const targetX = left + width / 2 < bodyWidth / 2 ? 0 : bodyWidth - width;
-      x = targetX;
-      setElStyle();
-      el.dispatchEvent(new CustomEvent('isMove', { detail: false }));
-    };
+      if (!isDragging) return
+      // 重新启用页面滚动
+      bodyScroll(true)
+      isDragging = false
+      el.style.transition = 'all 0.3s ease-out'
+      const { left, width } = el.getBoundingClientRect()
+      const bodyWidth = document.body.getBoundingClientRect().width
+      const targetX = left + width / 2 < bodyWidth / 2 ? 0 : bodyWidth - width
+      x = targetX
+      setElStyle()
+      el.dispatchEvent(new CustomEvent('isMove', { detail: false }))
+    }
 
-    el.addEventListener('touchstart', onTouchStart);
+    el.addEventListener('touchstart', onTouchStart)
     el.addEventListener('touchmove', onTouchMove, {
       passive: false,
-    });
-    el.addEventListener('touchend', onTouchEnd);
+    })
+    el.addEventListener('touchend', onTouchEnd)
 
-    el._onTouchStart = onTouchStart;
-    el._onTouchMove = onTouchMove;
-    el._onTouchEnd = onTouchEnd;
+    el._onTouchStart = onTouchStart
+    el._onTouchMove = onTouchMove
+    el._onTouchEnd = onTouchEnd
   },
 
   beforeUnmount(el) {
-    el.removeEventListener('touchstart', el._onTouchStart);
-    el.removeEventListener('touchmove', el._onTouchMove);
-    el.removeEventListener('touchend', el._onTouchEnd);
+    el.removeEventListener('touchstart', el._onTouchStart)
+    el.removeEventListener('touchmove', el._onTouchMove)
+    el.removeEventListener('touchend', el._onTouchEnd)
   },
 }
 export default DragTouch
